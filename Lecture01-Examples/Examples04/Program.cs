4dda5bfdd318e7d90ddeeed285e2b90978f8c734
adsd198654 <adsd198654@gmail.com>
2017-07-17 23:11:11 +0800

    Add lecture02 Example1

---
+++ Lecture01-Examples/Examples04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples04
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            float a = 0.1f;
+            float b = 0.2f;
+            float c = 0.3f;
+            float y = (0.1f / 0.2f) * (a + b);
+            Console.WriteLine(a + b == c);
+            Console.WriteLine(y);
+        }
+    }
+}
